838bd945c4eb2590ecc23516fec91b32239b0a7d
zhaixiaoou <zhaixiaoou@yxqiche.com>
2019-03-06 15:03:41 +0800

    test

--- app/src/main/java/com/zxo/testgit/MainActivity.java
+++ app/src/main/java/com/zxo/testgit/MainActivity.java
@@ -9,5 +9,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        final Paint mPaint = new Paint();
+//        mPaint.setColor(0xffff0000);
+//        mPaint.setStrokeWidth(10);
+//        setContentView(new View(this){
+//            @Override
+//            protected void onDraw(Canvas canvas) {
+//                super.onDraw(canvas);
+//                canvas.drawLine(0,0,getWidth(), getHeight(), mPaint);
+//            }
+//        });
+//        mWebView = findViewById(R.id.webview);
+//        mWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+//        mWebView.loadUrl("http://192.168.162.125/");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
     }
 }
