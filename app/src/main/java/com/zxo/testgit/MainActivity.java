c89ff6c2d494fb656a6130e4ac06968d10a93cb0
zhaixiaoou <zhaixiaoou@yxqiche.com>
2019-03-08 16:21:03 +0800

    add

--- app/src/main/java/com/zxo/testgit/MainActivity.java
+++ app/src/main/java/com/zxo/testgit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zxo.testgit;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        final Paint mPaint = new Paint();
+        final Paint mPaint = new Paint();
 //        mPaint.setColor(0xffff0000);
 //        mPaint.setStrokeWidth(10);
 //        setContentView(new View(this){
@@ -29,6 +30,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        //
+        //sfklksjf;
     }
 }
